14168242fee37fc9e3f30b208b1f66c54a350b92
ajbhandal <amarjit.bhandal@superchoice.com.au>
2023-03-03 12:37:45 +1100

    Remove unused imports

--- src/test/java/dev/aj/randomdatagenerator/service/ATOServiceTest.java
+++ src/test/java/dev/aj/randomdatagenerator/service/ATOServiceTest.java
@@ -3,12 +3,8 @@ package dev.aj.randomdatagenerator.service;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.springframework.beans.factory.annotation.Value;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class ATOServiceTest {
     private ATOService atoService;
